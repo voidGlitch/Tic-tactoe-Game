8b6cec7e0405f96e04219f25b6c0582f0ab1cdaf
voidGlitch <official.shreshthav@gmail.com>
2021-08-12 14:01:12 +0530

    created new board and square component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-//first rule of react a component must always return a single element as you can see there is empty<>
-   {/* or it is just a <React.Fragment> */}
- {/* it looks like html but it is js6 and below is reacts syntax */}
+// eslint-disable-next-line import/extensions
+import Board from "./Components/Board.js";
+// //first rule of react a component must always return a single element as you can see there is empty<>
+//    {/* or it is just a <React.Fragment> */}
+//  {/* it looks like html but it is js6 and below is reacts syntax */}
 const app = () =>{
- return ( <>
-
-  <h1>Welcome to React Parcel Micro App!</h1>
-  <p>Hard to get more minimal than this React app.</p>
-  <p> hey there</p>
-  <center>its center</center>
-</>
+ return (
+<div>
+  <h1>TIC TAC TOE Game</h1>
+  <Board />
+</div>
  )
 };
 export default app;
 
-//to run this command simply write npm run start 
+// to run this command simply write npm run start 
 // to stop the script ctrl+c
